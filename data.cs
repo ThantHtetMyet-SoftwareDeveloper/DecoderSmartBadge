bfa8434dd1125cda03921f9bd00a7727bce8fd81
Shannon <sherielchoo@gmail.com>
2021-06-22 12:28:32 +0800

    master

---
+++ data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecodeSmartBadgeData
+{
+    class data
+    {
+        public string DevEUI { get; set; }
+        public string SensorType { get; set; }
+        public string ModelCfg { get; set; }
+        public string MessageType { get; set; }
+        public string BeaconId { get; set; }
+        public string Payload_hex { get; set; }
+        public string Time { get; set; }
+
+
+    }
+}
